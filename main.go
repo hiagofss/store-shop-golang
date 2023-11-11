20c6b16e7d57233bfd1381e1e391ebdfcf837dec
Hiago Souza <hiagofss98@gmail.com>
2023-11-11 18:20:56 -0300

    Remove unused func

--- main.go
+++ main.go
@@ -31,14 +31,6 @@ func main() {
 	http.ListenAndServe("localhost:8000", nil)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	products := []Product{
-		{"Milk", "Milk is a white liquid produced by the mammary glands of mammals.", 2.45, 10},
-		{"Cheese", "Cheese is a dairy product, derived from milk and produced in wide ranges of flavors, textures and forms.", 3.99, 5},
-		{"Yogurt", "Yogurt, yoghurt or yoghourt is a food produced by bacterial fermentation of milk.", 1.99, 7}}
-	temp.ExecuteTemplate(w, "Index", products)
-}
-
 func renderProducts(w http.ResponseWriter, r *http.Request) {
 	db := connectDatabase()
 	productsList, err := db.Query("SELECT * FROM products")
